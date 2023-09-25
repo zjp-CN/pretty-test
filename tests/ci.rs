ecec4f94528ea0d01bf9a98343c562eca625399f
zjp <jiping_zhou@foxmail.com>
2023-09-25 23:30:39 +0800

    strip_ansi_escapes

--- tests/ci.rs
+++ tests/ci.rs
@@ -1,5 +1,7 @@
 use std::process::Command;
 
+use cargo_pretty_test::parsing::parse_stderr;
+
 #[test]
 fn check_stderr_stdout() {
     let output = Command::new("cargo")
@@ -8,5 +10,13 @@ fn check_stderr_stdout() {
         .unwrap();
     let stderr = String::from_utf8_lossy(&output.stderr);
     let stdout = String::from_utf8_lossy(&output.stdout);
-    println!("stderr = {stderr}\n\nstdout = {stdout}");
+    println!("stderr = {stderr:?}\n\nstdout = {stdout:?}");
+}
+
+#[test]
+fn check_stderr() {
+    let stderr = "\n     Finished test [optimized + debuginfo] target(s) in 0.20s
+     Running unittests src/lib.rs (target/debug/deps/torrust_tracker-8c4e3a20c54a4e7b)
+     Running unittests src/main.rs (target/debug/deps/torrust_tracker-5d2d33080cf6537c)";
+    dbg!(parse_stderr(stderr));
 }
